b73ca700b3bba96b1fe62ce6cf0e5327fcee1ed4
sh00tn1ck29 <maksimsavrygin21@gmail.comgit config --global user.name sh00tn1ck29git config --global user.email maksimsavrygin21@gmail.com>
2026-08-25 20:16:40 +0200

    feat: initial structure of the project

---
+++ .prettierrc.js
@@ -0,0 +1,31 @@
+export default {
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-pkg"],
+  singleQuote: true,
+  trailingComma: "all",
+  overrides: [
+    {
+      files: "*.json5",
+      options: {
+        parser: "json5",
+        quoteProps: "preserve",
+        singleQuote: false,
+        trailingComma: "none",
+      },
+    },
+    {
+      files: "*.md",
+      options: {
+        // @ts-expect-error: known property
+        organizeImportsSkipDestructiveCodeActions: true,
+      },
+    },
+    {
+      files: "package.json",
+      options: {
+        packageSortOrderPreset: "npm-plus",
+        packageSortOrder: ["name", "version", "description", "scripts"],
+        packageIgnoreSort: ["scripts"],
+      },
+    },
+  ],
+};
